eed0bdc61fe4221cefe29dce8bf47e7e814cf532
nexlice <auspiciouswho47@gmail.com>
2021-10-03 12:37:04 +0900

    shattering into functions/classses

--- Wonwhoo/fgsm_tutorial.py
+++ Wonwhoo/fgsm_tutorial.py
@@ -113,7 +113,10 @@ import matplotlib.pyplot as plt
 #for image saving
 import os
 from torchvision.utils import save_image
-
+#for model definition
+from LeNet_model_definition import Net
+#for fgsm_attack
+from fgsm_attack import fgsm_attack
 # NOTE: This is a hack to get around "User-agent" limitations when downloading MNIST datasets
 #       see, https://github.com/pytorch/vision/issues/3497 for more information
 from six.moves import urllib
@@ -185,23 +188,6 @@ use_cuda=True
 
 
 # LeNet Model definition
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
 
 # MNIST Test dataset and dataloader declaration
 test_loader = torch.utils.data.DataLoader(
@@ -244,18 +230,6 @@ model.eval()
 # 
 
 # In[ ]:
-
-
-# FGSM attack code
-def fgsm_attack(image, epsilon, data_grad):
-    # Collect the element-wise sign of the data gradient
-    sign_data_grad = data_grad.sign()
-    # Create the perturbed image by adjusting each pixel of the input image
-    perturbed_image = image + epsilon*sign_data_grad
-    # Adding clipping to maintain [0,1] range
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    # Return the perturbed image
-    return perturbed_image
 
 
 # Testing Function
